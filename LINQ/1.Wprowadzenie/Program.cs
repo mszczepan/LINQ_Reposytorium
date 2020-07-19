2fb31333bb9db5437494309168c451ba60b654cf
Mateusz <szczepan.mateusz@gmail.com>
2020-07-19 09:32:54 +0200

    1.Query syntax

--- LINQ/1.Wprowadzenie/Program.cs
+++ LINQ/1.Wprowadzenie/Program.cs
@@ -13,7 +13,24 @@ namespace _1.Wprowadzenie
         static void Main(string[] args)
         {
             var sciezka = @"c:\windows";
+
+            PokazDuzePlikiZLinq(sciezka);
+
+            Console.WriteLine("******************");
             PokazDuzePlikiBezLinq(sciezka);        }
+
+        private static void PokazDuzePlikiZLinq(string sciezka)
+        {
+            var zapytanie = from plik in new DirectoryInfo(sciezka).GetFiles()
+                            orderby plik.Length descending
+                            select plik;
+
+            foreach (var plik in zapytanie.Take(5))
+            {
+
+                Console.WriteLine($"{plik.Name,-20}:{plik.Length,20:N0}");
+            }
+        }
 
         private static void PokazDuzePlikiBezLinq(string sciezka)
         {
@@ -22,11 +39,7 @@ namespace _1.Wprowadzenie
 
             Array.Sort(pliki,new FileInfoComparer());
 
-            for (int i = 0; i < 4; i++)
-            {
-
-            }
-
+  
             for (var i=0; i<5;i++)
             {
                 Console.WriteLine($"{pliki[i].Name,-20}:{pliki[i].Length,20:N0}");
