8f9a7960c3f83a4ae5ff2579fe7de5a2461b5bb2
Mateusz <szczepan.mateusz@gmail.com>
2020-08-02 18:13:29 +0200

    2_Funkcje - IEnumerable

---
+++ LINQ/2_Funkcje/Pracownik.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Funkcje
+{
+    public class Pracownik
+    {
+        public int Id { get; set; }
+        public String Imie { get; set; }
+        public String Nazwisko { get; set; }
+
+    }
+}
---
+++ LINQ/2_Funkcje/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Funkcje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IEnumerable<Pracownik> programisci = new Pracownik[]
+            {
+                new Pracownik{Id=1,Imie="Marcin",Nazwisko="Kowlski" },
+                new Pracownik{Id=2,Imie="Tomek",Nazwisko="Sobota" },
+                new Pracownik{Id=3,Imie="Krzysztof",Nazwisko="Bosak" },
+                new Pracownik{Id=4,Imie="Marcin",Nazwisko="Maniura" },
+
+            };
+
+            IEnumerable<Pracownik> kierowcy = new List<Pracownik>
+            {
+                new Pracownik{Id=5,Imie="Andrzej",Nazwisko="Krzywy" },
+                new Pracownik{Id=6,Imie="Franciszek",Nazwisko="Bielas" },
+                new Pracownik{Id=7,Imie="Anna",Nazwisko="Szara" },
+            };
+
+           // foreach (var osoba in programisci)
+            // {
+
+            //    Console.WriteLine(osoba.Imie + " " + osoba.Nazwisko);
+
+            // }
+
+            IEnumerator<Pracownik> enumerator = kierowcy.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Imie);
+            }
+
+        }
+    }
+}
